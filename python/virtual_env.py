9dce2f8c2c66d837947389575f93c84d56687cf6
Aruna Vangapandu <arunavangapandu@mac.lan>
2025-09-04 12:39:25 -0700

    python content

---
+++ python/virtual_env.py
@@ -0,0 +1,14 @@
+# Virtual env ---> // same machine multiple projects, can create isolated env for each project
+# by logical separation using virtualenv (venv)
+# commands to create and activate virtual env
+# python -m venv project-v1 // project created for project-v1
+# source project-v1/bin/activate // activate the created virtual env
+#pip install jira
+#pip list | grep jira
+# deactivate // to deactivate the virtual env
+# PYPI  --> Python Package Index
+# https://pypi.org/ 
+# We use pip to install packages from PYPI
+
+
+
