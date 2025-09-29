bfb83e3c4402963d13a74310c5032677a3c04234
Aruna Vangapandu <arunavangapandu2016@gmail.com>
2025-09-29 09:50:53 -0700

    updated code

--- python/list-dict.py
+++ python/list-dict.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 
 #/repos/{owner}/{repo}/pulls
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
@@ -26,8 +26,8 @@ def get_active_status(server_name):
 server_name = 'server2'
 status = get_active_status(server_name)
 print(f"{server_name} status: {status}")
-
-def take_request(url):
+ """
+""" def take_request(url):
     response = requests.get(url)
     return response
 
@@ -36,7 +36,7 @@ if response.status_code == 200:
     print("Request was successful")
     pull_requests = response.json()
 
-    pr_creators = {}
+    pr_creators = {}clear
     for pr in pull_requests:
         creator = pr['user']['login']
         if creator in pr_creators:
@@ -44,5 +44,72 @@ if response.status_code == 200:
         else:
             pr_creators[creator] = 1
 
-    print(pr_creators)
+    print(pr_creators) """
     
+
+""" def build(snacks):
+        for i in range(len(snacks)):
+            item = snacks[i]
+            name = item[0]
+            calories = item[1]
+            print(f'#{i+1} {name} : {calories}')
+
+    # list of tuples
+snacks = [('bacon', 350), 
+              ('donut', 200),
+              ('muffin', 100)]
+    
+build(snacks)
+
+def build(snacks):
+      for i, item, value in enumerate(snacks, i):
+            print(f'# i {item}: {value}')
+
+flavor_list = ['vanila', 'choclate', 'pecan', 'strawberry']
+for flavor in flavor_list:
+      print(f'{flavor} is delicious')
+
+
+def max_count(names):
+    max_count = 0
+    for i in range(len(names)):
+          if len(names[i]) > max_count:
+                longest_string = names[i]
+                max_count = len(names[i])
+    return max_count
+
+names = ['Cecilia', 'Lise', 'Marie']
+print(max_count(names))
+
+
+def sort_numbers(numbers):
+      numbers.sort()
+      print(numbers)
+
+numbers = [11, 68, 70, 86]
+sort_numbers(numbers) """
+
+class Tool:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __repr__(self):
+        return f'Tool({self.name}, {self.weight})'
+    
+tools = [
+    Tool('level', 3.5),
+    Tool('hammer', 1.25),
+    Tool('screwdriver', 0.5),
+    Tool('chisel', 0.25)
+]
+print('Unsorted:', str(tools))
+tools.sort(key=lambda x: x.name)
+print('\nSorted: ', tools)
+
+tools.sort(key = lambda x: (x.weight, x.name))
+for tool in tools:
+   print(tool.__repr__()) 
+
+
+places = ['home', 'work', 'New York', 'Paris']
